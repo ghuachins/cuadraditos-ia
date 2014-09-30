807d024d15228ff558d8f6d17b096060eba483ef
ghuachins@gmail.com <ghuachins@gmail.com>
2014-09-30 16:44:28 -0500

    Creacion completa de la interfaz
    Creacion de las lineas y circulos del juego de Cuadraditos

--- graficos.lisp
+++ graficos.lisp
@@ -9,9 +9,10 @@
 
 ;---------------------------------------------------
 
-;(inicializar-juego)
+(inicializar-juego2)
 
 ;---------------------------------------------------
+
 
 (defvar *Posiciones*)
 (defvar *colorFicha*)
@@ -20,18 +21,8 @@
 (defvar *jugada*)
 (defvar *turno*)
 
-(setq *Posiciones* '(
-  ( (10 10) (110 110) ) ; 1
-  ( (160 10) (260 110) ) ; 2
-  ( (310 10) (410 110) ) ; 3
-  ( (10 160) (110 260) ) ; 4
-  ( (160 160) (260 260) ) ; 5
-  ( (310 160) (410 260) ) ; 6
-  ( (10 310) (110 410) ) ; 7
-  ( (160 310) (260 410) ) ; 8
-  ( (310 310) (410 410) ) ; 9
-  )
-
+(setq 
+  *colorLinea* '"#ccd2c8"
   *colorFicha* '"#394034"
   *colorHumano* '"#ff2f70"
   *colorOrdenador* '"#0083e8" )
@@ -39,25 +30,118 @@
 (defun tablero()
 	(with-ltk ()
 		(let* (
-			(sc (make-instance 'canvas :height 420 :width 420))
-			(lienzo (canvas sc))
-			; Caminos por el tablero
-			(linea1 (create-line lienzo (list 60 60 360 60)))
+					(sc (make-instance 'canvas :height 520 :width 520))
+					(lienzo (canvas sc))
+					; Caminos por el tablero
+					
+					;(linea2 (create-line lienzo (list 60 360 360 360)))
+
+                    (ovalo1 (create-oval lienzo 50 50 70 70))
+					(ovalo2 (create-oval lienzo 250 50 270 70))
+					(ovalo3 (create-oval lienzo 450 50 470 70))
+
+					(linea1 (create-line lienzo (list 80 60 240 60) ))
+					(linea2 (create-line lienzo (list 280 60 440 60) ))
+
+					(linea3 (create-line lienzo (list 60 80 60 240) ))
+					(linea4 (create-line lienzo (list 260 80 260 240) ))
+					(linea5 (create-line lienzo (list 460 80 460 240) ))
+
+ 					(ovalo4 (create-oval lienzo 50 250 70 270))
+					(ovalo5 (create-oval lienzo 250 250 270 270))
+					(ovalo6 (create-oval lienzo 450 250 470 270))
+
+					(linea6 (create-line lienzo (list 80 260 240 260) ))
+					(linea7 (create-line lienzo (list 280 260 440 260) ))
+
+					(linea8 (create-line lienzo (list 60 280 60 440) ))
+					(linea9 (create-line lienzo (list 260 280 260 440) ))
+					(linea10 (create-line lienzo (list 460 280 460 440) ))
+
+					(ovalo7 (create-oval lienzo 50 450 70 470))
+					(ovalo8 (create-oval lienzo 250 450 270 470))
+					(ovalo9 (create-oval lienzo 450 450 470 470))
+
+					(linea11 (create-line lienzo (list 80 460 240 460) ))
+					(linea12 (create-line lienzo (list 280 460 440 460) ))
 		
 			 )
 
 		(pack sc :expand 1 :fill :both)
 		(wm-title *tk* "Cuadraditos 3X3")
 
-		
-	    
+		(bind lienzo "<ButtonPress-1>"
+			(lambda (evento)
+				
+				(itemconfigure lienzo linea1 :width 20)
+			(itemconfigure lienzo linea1 :fill *colorHumano*)		
+
+			)
+
+		)
+				    
 
 		; Grosor de los caminos
 		(itemconfigure lienzo linea1 :width 8)
-		
+		(itemconfigure lienzo linea1 :fill *colorLinea*)
+	    (itemconfigure lienzo linea2 :width 8)
+		(itemconfigure lienzo linea2 :fill *colorLinea*)
+         
+        (itemconfigure lienzo linea3 :width 8)
+		(itemconfigure lienzo linea3 :fill *colorLinea*)
+	    (itemconfigure lienzo linea4 :width 8)
+		(itemconfigure lienzo linea4 :fill *colorLinea*)
+		(itemconfigure lienzo linea5 :width 8)
+		(itemconfigure lienzo linea5 :fill *colorLinea*)
+
+         
+		(itemconfigure lienzo ovalo1 :fill *colorFicha*)
+		(itemconfigure lienzo ovalo1 :width 3)
+		(itemconfigure lienzo ovalo2 :fill *colorFicha*)
+		(itemconfigure lienzo ovalo2 :width 3)
+		(itemconfigure lienzo ovalo3 :fill *colorFicha*)
+		(itemconfigure lienzo ovalo3 :width 3)
+
+		(itemconfigure lienzo ovalo4 :fill *colorFicha*)
+		(itemconfigure lienzo ovalo4 :width 3)
+		(itemconfigure lienzo ovalo5 :fill *colorFicha*)
+		(itemconfigure lienzo ovalo5 :width 3)
+		(itemconfigure lienzo ovalo6 :fill *colorFicha*)
+		(itemconfigure lienzo ovalo6 :width 3)
+
+
+		(itemconfigure lienzo linea6 :width 8)
+		(itemconfigure lienzo linea6 :fill *colorLinea*)
+	    (itemconfigure lienzo linea7 :width 8)
+		(itemconfigure lienzo linea7 :fill *colorLinea*)
+         
+        (itemconfigure lienzo linea8 :width 8)
+		(itemconfigure lienzo linea8 :fill *colorLinea*)
+	    (itemconfigure lienzo linea9 :width 8)
+		(itemconfigure lienzo linea9 :fill *colorLinea*)
+		(itemconfigure lienzo linea10 :width 8)
+		(itemconfigure lienzo linea10 :fill *colorLinea*)
+
+
+
+
+
+		(itemconfigure lienzo ovalo7 :fill *colorFicha*)
+		(itemconfigure lienzo ovalo7 :width 3)
+		(itemconfigure lienzo ovalo8 :fill *colorFicha*)
+		(itemconfigure lienzo ovalo8 :width 3)
+		(itemconfigure lienzo ovalo9 :fill *colorFicha*)
+		(itemconfigure lienzo ovalo9 :width 3)
+
+		(itemconfigure lienzo linea11 :width 8)
+		(itemconfigure lienzo linea11 :fill *colorLinea*)
+	    (itemconfigure lienzo linea12 :width 8)
+		(itemconfigure lienzo linea12 :fill *colorLinea*)
+
          )
       )
 
 )
+
 (tablero)
 
